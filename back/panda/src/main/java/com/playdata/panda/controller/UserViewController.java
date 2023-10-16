de71e4bb7edbc8072e4a96b376a54bbfe38f5887
Kim Suyong <130054385+ghktn@users.noreply.github.com>
2023-10-16 00:40:01 +0000

    Revert "feat #501 지역 분류"

--- back/panda/src/main/java/com/playdata/panda/controller/UserViewController.java
+++ back/panda/src/main/java/com/playdata/panda/controller/UserViewController.java
@@ -1,43 +1,27 @@
 package com.playdata.panda.controller;
 
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.playdata.panda.dto.RegionSd;
-import com.playdata.panda.service.RegionService;
-import com.playdata.panda.service.UserService;
-import com.playdata.panda.service.UserServiceImpl;
-
-import lombok.RequiredArgsConstructor;
-
-@RequiredArgsConstructor
 @Controller
 public class UserViewController {
 
-	private final UserService userService;
-	private final RegionService regionService;
+    /**
+     * @return 회원가입 페이지 화면을 리턴한다.
+     */
+    @GetMapping("/signup")
+    public String signupPageView() {
+        return "member/register";
+    }
 
-	/**
-	 * @return 회원가입 페이지 화면을 리턴한다.
-	 */
-	@GetMapping("/signup")
-	public String signupPageView(Model model) {
-		List<RegionSd> regionSdList = regionService.findRegionSdList();
-		model.addAttribute("regionSdList", regionSdList);
-		return "member/register";
-	}
+    @GetMapping("/login")
+    public String loginPageView() {
+        return "member/login2";
+    }
 
-	@GetMapping("/login")
-	public String loginPageView() {
-		return "member/login2";
-	}
-
-	@GetMapping("/mypage")
-	public String myPageView() {
-		return "member/my-page";
-	}
+    @GetMapping("/mypage")
+    public String myPageView() {
+        return "member/my-page";
+    }
 
 }
