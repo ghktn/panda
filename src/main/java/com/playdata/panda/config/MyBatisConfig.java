0fd43c58775cfa96174a1aff24af7ea99f6f9c48
Kim Suyong <rlaghkdyd99@naver.com>
2023-10-17 17:13:41 +0900

    feat #로그아웃 구현
    header.html에서 thymeleaf 문법인 th:if 와 th:unless를 활용해서 구현

--- src/main/java/com/playdata/panda/config/MyBatisConfig.java
+++
@@ -1,9 +0,0 @@
-package com.playdata.panda.config;
-
-import org.apache.ibatis.annotations.Mapper;
-import org.springframework.stereotype.Repository;
-
-@Mapper
-@Repository
-public interface MyBatisConfig {
-}
