9fc9cd4034fae6d2dbeb543eef93596f500e427c
Kim Suyong <rlaghkdyd99@naver.com>
2023-10-17 19:30:25 +0900

    feat# LoginSuccessDTO 수정

--- src/main/java/com/playdata/panda/dto/LoginSuccessDTO.java
+++ src/main/java/com/playdata/panda/dto/LoginSuccessDTO.java
@@ -1,17 +1,24 @@
 package com.playdata.panda.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class LoginSuccessDTO {
 
-	private long id;
-	private String userId;
+    private long id;
+    private String userId;
 
-	public LoginSuccessDTO(long id, String userId) {
-		super();
-		this.id = id;
-		this.userId = userId;
-	}
+    public LoginSuccessDTO(long id, String userId) {
+        super();
+        this.id = id;
+        this.userId = userId;
+    }
 
-	public static LoginSuccessDTO create(long id, String userId) {
-		return new LoginSuccessDTO(id, userId);
-	}
+    public static LoginSuccessDTO create(long id, String userId) {
+        return new LoginSuccessDTO(id, userId);
+    }
 }
